fc8cd82cc3ea88649fbb9b80fd8ca4076ca84538
josxle <joselopezespinoza5@gmail.com>
2025-04-17 00:47:03 -0700

    logout

--- src/componentes/welcome/welcome.jsx
+++ src/componentes/welcome/welcome.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ComingSoon from '../../images/coming_soon.png';
 import './welcome.css'
+import josxle from '../../images/josxle.png';
+import logout from '../../images/logout.png';
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
     return (
         <div>
-            {/* aventarme el header con logout
-
-            <header>
-                asd
+            <header className='hd'>
+                <div className='left'>
+                    <img className='logo' src={josxle} alt="logo" />
+                </div>
+                <div className='right'>
+                    <Link to="/">
+                        <img className='logout' src={logout} alt="logout" />
+                    </Link>
+                </div>
             </header>
             
-            */}
+
             <img className='ha' src={ComingSoon} alt="ha" />
         </div>
     );
